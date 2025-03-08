11182d29abdc50dcc71fdc9b8d3948cde065a61f
Lucas Mosca <lucas.mosca@madeinweb.com.br>
2025-03-08 20:46:42 -0300

    Feature: Add base test field.

--- flutter_base_project/lib/ui/system_design/base_widgets/base_text_field.dart
+++ flutter_base_project/lib/ui/system_design/base_widgets/base_text_field.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_base_project/base/providers/theme_provider.dart';
 import 'package:flutter_base_project/utils/constants/app_colors.dart';
+import 'package:flutter_base_project/utils/constants/app_sizes.dart';
 
 class BaseTextField extends StatefulWidget {
   const BaseTextField({
@@ -16,12 +18,32 @@ class BaseTextField extends StatefulWidget {
     this.fontHeight,
     this.fontLightColor,
     this.fontDarkColor,
+    this.borderRadius,
+    this.borderLightColor,
+    this.borderDarkColor,
+    this.borderFocusedLightColor,
+    this.borderFocusedDarkColor,
+    this.borderErrorLightColor,
+    this.borderErrorDarkColor,
+    this.borderEnabledLightColor,
+    this.borderEnabledDarkColor,
+    this.margin,
+    this.keyboardType,
+    this.maxLines,
+    this.onSubmitted,
+    this.controller,
+    this.focusNode,
+    this.inputFormatters,
+    this.textInputAction,
+    this.borderWidth,
   });
 
   final double? height;
   final double? width;
   final String hintText;
+  final EdgeInsets? margin;
   final ValueChanged<String> onTextChanged;
+  final double? borderWidth;
 
   final String? text;
   final double? fontSize;
@@ -31,12 +53,36 @@ class BaseTextField extends StatefulWidget {
   final Color? fontLightColor;
   final Color? fontDarkColor;
 
+  final double? borderRadius;
+
+  final Color? borderLightColor;
+  final Color? borderDarkColor;
+
+  final Color? borderFocusedLightColor;
+  final Color? borderFocusedDarkColor;
+
+  final Color? borderErrorLightColor;
+  final Color? borderErrorDarkColor;
+
+  final Color? borderEnabledLightColor;
+  final Color? borderEnabledDarkColor;
+
+  final TextInputType? keyboardType;
+  final int? maxLines;
+  final void Function(String)? onSubmitted;
+
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextInputAction? textInputAction;
+
   @override
   State<BaseTextField> createState() => _BaseTextFieldState();
 }
 
 class _BaseTextFieldState extends State<BaseTextField> {
   final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
@@ -47,6 +93,7 @@ class _BaseTextFieldState extends State<BaseTextField> {
   @override
   void dispose() {
     _controller.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
@@ -56,13 +103,22 @@ class _BaseTextFieldState extends State<BaseTextField> {
     final textStyle = _textStyle(context);
     _controller.text = widget.text ?? "";
 
-    return TextField(
-      controller: _controller,
-      style: _defaultTextStyle(textStyle, isDarkMode),
-      decoration: _hintStyle(textStyle, isDarkMode),
-      onChanged: _onTextChanged,
-      maxLines: null,
-      keyboardType: TextInputType.multiline,
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      margin: widget.margin,
+      child: TextField(
+        focusNode: widget.focusNode ?? _focusNode,
+        controller: widget.controller ?? _controller,
+        style: _defaultTextStyle(textStyle, isDarkMode),
+        decoration: _hintStyle(textStyle, isDarkMode),
+        onChanged: _onTextChanged,
+        maxLines: widget.maxLines,
+        keyboardType: widget.keyboardType ?? TextInputType.text,
+        textInputAction: widget.textInputAction ?? TextInputAction.next,
+        onSubmitted: widget.onSubmitted,
+        inputFormatters: widget.inputFormatters,
+      ),
     );
   }
 
@@ -92,12 +148,32 @@ class _BaseTextFieldState extends State<BaseTextField> {
 
   InputDecoration _hintStyle(TextStyle? textStyle, bool isDarkMode) {
     return InputDecoration(
-      border: InputBorder.none,
+      border: _defaultBorder(isDarkMode),
+      enabledBorder: _defaultBorder(isDarkMode),
+      focusedBorder: _defaultBorder(isDarkMode),
+      errorBorder: _defaultBorder(isDarkMode),
       hintText: widget.hintText,
       labelStyle: _defaultTextStyle(textStyle, isDarkMode),
       hintStyle: _labelTextStyle,
       counter: const Offstage(),
     );
+  }
+
+  OutlineInputBorder _defaultBorder(bool isDarkMode) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(
+        widget.borderRadius ?? AppSizes.borderRadiusLg,
+      ),
+      borderSide: BorderSide(
+        color: _borderColor(isDarkMode),
+        width: widget.borderWidth ?? 1.0,
+      ),
+    );
+  }
+
+  Color _borderColor(bool isDarkMode) {
+    return (isDarkMode ? widget.borderDarkColor : widget.borderLightColor) ??
+        AppColors.black;
   }
 
   TextStyle get _labelTextStyle =>
